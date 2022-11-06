974b5cb6bb980a8ccc8100601180d01649859c31
mazeoff <nikonorov@rptp.org>
2022-11-06 17:21:09 +0300

    добавлен bootstrap

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,23 +12,35 @@ class IndexController extends AbstractController
     public function index(): Response
     {
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'title' => 'IndexController',
         ]);
     }
 
     #[Route('/services', name: 'services')]
     public function services(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'SERVICES',
+        return $this->render('index/services.html.twig', [
+            'title' => 'SERVICES',
+            'items' => '10',
         ]);
     }
 
     #[Route('/services/item/{id<\d+>}', name: 'servicesItem')]
     public function servicesItem(int $id): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'SERVICES ITEM' . $id,
+        return $this->render('index/servicesItem.html.twig', [
+            'title' => 'SERVICES ITEM' . $id,
+            'description' => 'description',
+            'price' => '1000'
+        ]);
+    }
+
+    #[Route('/transactions', name: 'transactions')]
+    public function transactions(): Response
+    {
+        return $this->render('index/transactions.html.twig', [
+            'title' => 'Ваши транзакции',
+            'items' => '10'
         ]);
     }
 }
